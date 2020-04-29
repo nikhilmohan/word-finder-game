4e041e599b1c3e21f4beec1d561f4927573eebe2
Nikhil Mohan <nikhil_mohan@infosys.com>
2020-04-29 15:50:22 +0530

    initial commit

---
+++ src/main/java/com/learningenglish/wordfinder/WordFinderApplication.java
@@ -0,0 +1,13 @@
+package com.learningenglish.wordfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordFinderApplication.class, args);
+	}
+
+}
